1d25a8928e644210da5b03fc73bc68e6ed4c36fc
winkel-titouan <titouan.winkel@edu.univ-fcomte.fr>
2024-12-04 11:29:26 +0100

    modif du js : bouton fonctionnels

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,17 @@
 // *** CAROUSEL ***
 // TODO
+
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+
+
+if (carousel) {
+    prevButton.addEventListener("click", () => {
+        carousel.scrollBy({ left: -300, behavior: 'smooth' });
+    });
+    nextButton.addEventListener("click", () => {
+        carousel.scrollBy({ left: 300, behavior: 'smooth' });
+    });
+}
